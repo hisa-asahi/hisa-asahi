a26940df6fce248dfae073d136e1d82cfd46848c
一宮　久展 <@LAPTOP-FG0GB94K>
2025-11-19 22:28:21 +0900

    修正完了

--- lesson java/src/curriculum_B/Question3.java
+++ lesson java/src/curriculum_B/Question3.java
@@ -8,48 +8,47 @@ public class Question3 {
 		for (int a = 1; a <= 10; a++) {
 			System.out.println(a);
 		}
-		
+
 		for (int b = 2; b <= 20; b += 2) {
 			System.out.println(b);
 		}
-		
+
 		for (int c = 10; c >= 1; c--) {
 			System.out.println(c);
 		}
-		
+
 		int d = 0;
-		for (int e = 1; e <= 100; e++) { 
-			d = d+ e;
+		for (int e = 1; e <= 100; e++) {
+			d = d + e;
 			System.out.println(d);
 		}
-		
+
 		for (int f = 1; f <= 5; f++) {
 			for (int aa = 1; aa <= 5; aa++) {
 				if (f >= aa)
 					System.out.print("*");
-				}
+			}
 			System.out.println();
 		}
-			
-		
+
 		int g = 1;
 		while (g <= 10) {
 			System.out.println(g);
 			g++;
 		}
-		
+
 		int h = 2;
 		while (h <= 20) {
 			System.out.println(h);
 			h += 2;
 		}
-		
+
 		int i = 10;
 		while (i >= 1) {
 			System.out.println(i);
 			i--;
 		}
-		
+
 		int j = 1;
 		int k = 0;
 		while (j <= 100) {
@@ -57,25 +56,27 @@ public class Question3 {
 			j++;
 		}
 		System.out.println(k);
-		
+
 		Scanner scanner = new Scanner(System.in);
-		int l = scanner.nextInt();
-		scanner.close();
-		while (l == 0) {
-		System.out.println("終了しました");
-		break;
+		while (true) {
+			int l = scanner.nextInt();
+			if (l == 0) {
+				scanner.close();
+				System.out.println("終了しました");
+				break;
+			}
 		}
-		
-		
+
 		for (int m = 1; m <= 9; m++) {
-		for (int n = 1; n <= 9; n++) {
-			System.out.print(m + "*" + n + "=" + n * m);
-			if (n < 9) {System.out.print("||");
+			for (int n = 1; n <= 9; n++) {
+				System.out.print(m + "*" + n + "=" + n * m);
+				if (n < 9) {
+					System.out.print("||");
+				}
+			}
+			System.out.println();
 		}
-		}
-		System.out.println();
-		}
-		
+
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
